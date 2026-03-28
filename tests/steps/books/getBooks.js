5878dce521cbe7a9f94d6826f9ec0dc7366e5342
vladvladovic141 <vladvladovic141@yandex.ru>
2026-03-28 20:38:19 +0200

    Do better

--- tests/steps/books/getBooks.js
+++ tests/steps/books/getBooks.js
@@ -4,7 +4,7 @@ export async function getBooks() {
     it('Retrieve list of all books', async function () {
         await request(this, 'GET', `/books?`, undefined, true,
             {
-                statusCode: 304,
+                statusCode: 200,
                 // expectedValues: [
                 //     {path: '0.id', value: 10},
                 //     {path: '6.id', value: 7},
